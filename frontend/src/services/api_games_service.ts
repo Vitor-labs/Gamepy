f2276f85758c3254f5568dbf3ed35cc602da829c
Vitor Hugo <v02hx10@gmail.com>
2023-02-12 22:41:22 -0300

    feat: refac api_games_service

--- frontend/src/services/api_games_service.ts
+++ frontend/src/services/api_games_service.ts
@@ -1,5 +1,17 @@
 import axios from "axios";
 
+interface Game {
+    id: number;
+    name: string;
+    price: number;
+    score: number;
+    publisher: string;
+    pub_date: string;
+    cover: File;
+    summary: string;
+    genre: string;
+}
+
 export function get_games() {
     return axios.get('http://127.0.0.1:8000/games/')
         .then(res => {
@@ -7,32 +19,43 @@ export function get_games() {
         })
 }
 
-export function add_game(game: object) {
+export function add_game(game: Game) {
     return axios.post('http://127.0.0.1:8000/games/',
         {
-            patient_id: null,
-            first_name: game.first_name.value,
-            last_name: game.last_name.value,
-            blood: game.blood.value,
+            id: null,
+            name: 'shadow of war',
+            price: 230.55,
+            score: 12,
+            publisher: 'square_inix',
+            pub_date: '12/02/2012',
+            cover: 'some/path/to/url',
+            summary: 'loren ipsum, lorem lorem ipsum ipsum',
+            genre: 'ACT'
         })
         .then(res => {
             return res.data
         })
 }
 
-export function edit_game(id: number, patient: object) {
-    return axios.put('http://127.0.0.1:8000/patient/' + id + '/',
+export function edit_game(id: number, game: Game) {
+    return axios.put('http://127.0.0.1:8000/game/' + id + '/',
         {
-            first_name: patient.first_name.value,
-            last_name: patient.last_name.value,
-            blood: patient.blood.value,
+            'id': game.id,
+            'name': game.name,
+            'price': game.price,
+            'score': game.score,
+            'publisher': game.publisher,
+            'pub_date': game.pub_date,
+            'cover': game.cover,
+            'summary': game.summary,
+            'genre': game.genre
         })
         .then(res => {
             return res.data
         })
 }
 
-export function del_game(id) {
+export function del_game(id: number) {
     return axios.delete('http://127.0.0.1:8000/game/' + id + '/')
         .then(res => {
             return res.data
