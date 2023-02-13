9b60e933e1909c7a52255bcd3ab55989b1e3855d
Vitor Hugo <v02hx10@gmail.com>
2023-02-12 22:04:49 -0300

    build: containers and deps

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -90,8 +90,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('POSTGRES_DB'), 
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
